064b813bdd949bd086128d2d10de60475792559e
insidesjsj <tjdwns428@naver.com>
2024-04-05 23:21:14 +0900

    20240405 지역 리스트 접근성 개성 li태그 tab으로 이동할 수 있도록

--- src/component/WeatherHeader.tsx
+++ src/component/WeatherHeader.tsx
@@ -37,10 +37,11 @@ const WeatherHeader: React.FC = () => {
         <div>
             <div className="pt-5 flex justify-between items-center">
                 <div className="w-1/6 font-['SokchoBadaDotum'] text-3xl">
-                        <h1><span className="cursor-pointer" onClick={() => window.location.reload()}>날씨의 연인</span></h1>
+                    <h1><span className="cursor-pointer" onClick={() => window.location.reload()}>날씨의 연인</span></h1>
                 </div>
                 <div className="w-1/4">
-                    <SearchGroup divClassName="w-full" searchSize="30px" onFocus={handleFocus} onBlur={handleBlur} readonly={true} />
+                    <SearchGroup divClassName="w-full" searchSize="30px" onFocus={handleFocus} onBlur={handleBlur}
+                                 readonly={true}/>
                 </div>
                 <div className="w-1/6 items-center">
 
@@ -53,27 +54,48 @@ const WeatherHeader: React.FC = () => {
                 <ModalContents>
                     <SearchGroup divClassName="w-full mb-2" searchSize="30px" onChange={search?.onChange}
                                  value={search?.search}/>
-                    <div className="h-3/4">
-                        <ul>
-                            {search?.regionData?.slice(0,10).map((item, index) => (
-                                <li
-                                    key={item.id}
-                                    className={`flex items-center text-left p-3 cursor-pointer ${hoveredItem === index ? 'bg-gray-200' : 'bg-white'}`}
-                                    onClick={() => clickRegion(item.combinedRegion, Number(item.nx), Number(item.ny), item.midRegionCode, item.midLandRegionCode)}
-                                    onMouseEnter={() => setHoveredItem(index)}
-                                    onMouseLeave={() => setHoveredItem(null)}
-                                >
-                                    <hr />
-                                    <p>{item.combinedRegion}</p>
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                    <button className="my-auto w-full h-14 bg-red-500 rounded-2xl text-white" onClick={handleBlur}>돌아가기</button>
+                        <div className="h-3/4 overflow-hidden overflow-y-auto">
+                            <ul>
+                                {search?.regionData?.map((item, index) => (
+                                    <li
+                                        key={item.id}
+                                        className={`flex items-center text-left p-3 cursor-pointer ${hoveredItem === index ? 'bg-gray-200' : 'bg-white'}`}
+                                        onClick={() => clickRegion(item.combinedRegion, Number(item.nx), Number(item.ny), item.midRegionCode, item.midLandRegionCode)}
+                                        onMouseEnter={() => setHoveredItem(index)}
+                                        onMouseLeave={() => setHoveredItem(null)}
+                                        // tabIndex 속성 추가
+                                        tabIndex={0}
+                                        // 키보드 이벤트 처리
+                                        onKeyDown={(e) => {
+                                            // Enter 키 또는 Space 키를 눌렀을 때 클릭 이벤트 발생
+                                            if (e.key === 'Enter' || e.key === ' ') {
+                                                clickRegion(item.combinedRegion, Number(item.nx), Number(item.ny), item.midRegionCode, item.midLandRegionCode);
+                                            }
+                                            // 화살표 키를 눌렀을 때 위 또는 아래로 이동
+                                            else if (e.key === 'ArrowUp') {
+                                                setHoveredItem((prevIndex) => Math.max(prevIndex ?? 0 - 1, 0));
+                                            } else if (e.key === 'ArrowDown') {
+                                                setHoveredItem((prevIndex) => Math.min(prevIndex ?? 0 + 1, (search?.regionData?.length ?? 0) - 1));
+                                            }
+                                        }}
+                                        // 포커스를 받았을 때 배경색 변경
+                                        onFocus={() => setHoveredItem(index)}
+                                        // 포커스를 잃었을 때 배경색 변경 제거
+                                        onBlur={() => setHoveredItem(null)}
+                                    >
+                                        <p>{item.combinedRegion}</p>
+                                        <hr/>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    <button className="my-auto w-full h-14 bg-red-500 rounded-2xl text-white mt-5"
+                            onClick={handleBlur}>돌아가기
+                    </button>
                 </ModalContents>
             </Modal>
         </div>
-    )
+    );
 }
 
 export default WeatherHeader;
