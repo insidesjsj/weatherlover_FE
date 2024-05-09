d0911615df28eda69767dc069cc88b3d65717404
insidesjsj <tjdwns428@naver.com>
2024-05-10 02:12:14 +0900

    반응형 웹을 위한 테일윈드 미디어쿼리 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,13 @@ module.exports = {
       "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    screens: {
+      'sm': {'max': '639px'},
+      'ml': {'max': '767px'},
+      'lg': {'max': '1023px'},
+      'xl': {'max': '1279px'},
+      '2xl': {'max': '1535px'},
+    },
     extend: {},
   },
   plugins: [],
