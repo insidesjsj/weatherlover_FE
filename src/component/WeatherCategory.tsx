9fa9dac9dd8e7e367d0925efe75edb88f457cde1
insidesjsj <tjdwns428@naver.com>
2024-07-01 15:25:56 +0900

    modify: 카테고리 접근성 수정 버튼 색상 변경, 크기 변경

--- src/component/WeatherCategory.tsx
+++ src/component/WeatherCategory.tsx
@@ -1,4 +1,4 @@
-import React, {FC} from 'react';
+import React, {FC, useState} from 'react';
 import {Button} from './Button';
 import {useRecoilState, useRecoilValue, useSetRecoilState} from 'recoil';
 import {categoryState} from '../recoilState';
@@ -8,22 +8,18 @@ import {categoryState} from '../recoilState';
 }*/
 const WeatherCategory: FC = () => {
     const [category, setCategory] = useRecoilState(categoryState);
-
+    const basic = 'w-[75px] p-2 border-none rounded-2xl bg-white'
+    const clicked = 'w-[75px] p-2 border-none rounded-2xl bg-blue-600 text-white'
     const onClickButton = (category: string) => {
         setCategory(category) // 여기에서 선택한 카테고리에 따라 필요한 로직을 수행할 수 있습니다.
     }
     return (
         <div className="font-['SUITE-Regular'] mt-2 ml-auto mr-auto w-3/6 flex justify-around text-lg">
-            <Button className={`p-1 border-2 rounded-2xl ${category === '온도' && 'bg-blue-500 text-white'}`}
-                    text="온도" onClick={() => onClickButton('온도')}/>
-            <Button className={`p-1 border-2 rounded-2xl ${category === '옷차림' && 'bg-blue-500 text-white'}`}
-                    text="옷차림" onClick={() => onClickButton('옷차림')}/>
-            <Button className={`p-1 border-2 rounded-2xl ${category === '습도' && 'bg-blue-500 text-white'}`}
-                    text="습도" onClick={() => onClickButton('습도')}/>
-            <Button className={`p-1 border-2 rounded-2xl ${category === '강수' && 'bg-blue-500 text-white'}`}
-                    text="강수" onClick={() => onClickButton('강수')}/>
-            <Button className={`p-1 border-2 rounded-2xl ${category === '바람' && 'bg-blue-500 text-white'}`}
-                    text="바람" onClick={() => onClickButton('바람')}/>
+            <Button className={category === '온도' ? clicked : basic} text="온도" onClick={() => onClickButton('온도')}/>
+            <Button className={category === '옷차림' ? clicked : basic} text="옷차림" onClick={() => onClickButton('옷차림')}/>
+            <Button className={category === '습도' ? clicked : basic} text="습도" onClick={() => onClickButton('습도')}/>
+            <Button className={category === '강수' ? clicked : basic} text="강수" onClick={() => onClickButton('강수')}/>
+            <Button className={category === '바람' ? clicked : basic} text="바람" onClick={() => onClickButton('바람')}/>
         </div>
     )
 }
